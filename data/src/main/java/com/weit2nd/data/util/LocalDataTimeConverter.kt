2c6b4cb8ce2750d045fe32d2bc09ea500642dad0
minseonglove <qnfpdlr124@naver.com>
2024-06-02 18:55:17 +0900

    ROFO-16 feat: 네트워크 객체 의존성 주입 정의

---
+++ data/src/main/java/com/weit2nd/data/util/LocalDataTimeConverter.kt
@@ -0,0 +1,27 @@
+package com.weit2nd.data.util
+
+import com.squareup.moshi.FromJson
+import com.squareup.moshi.JsonQualifier
+import com.squareup.moshi.ToJson
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+
+@Retention
+@JsonQualifier
+annotation class StringToLocalDateTime
+
+class LocalDateTimeConverter {
+
+    private val dateFormat = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")
+
+    @ToJson
+    fun toJson(@StringToLocalDateTime localDate: LocalDateTime): String {
+        return dateFormat.format(localDate)
+    }
+
+    @FromJson
+    @StringToLocalDateTime
+    fun fromJson(source: String): LocalDateTime {
+        return LocalDateTime.parse(source, dateFormat)
+    }
+}
