dc302f6d8282b796a3ab1fff365f5a9fb1b7b3f1
pingu244 <pingu244@naver.com>
2024-06-11 14:34:33 +0900

    ROFO-24 refactor: 중복 검사 중에 입력값이 변할 경우 api 취소

--- presentation/src/main/java/com/weit2nd/presentation/ui/signup/SignUpViewModel.kt
+++ presentation/src/main/java/com/weit2nd/presentation/ui/signup/SignUpViewModel.kt
@@ -1,11 +1,14 @@
 package com.weit2nd.presentation.ui.signup
 
 import android.net.Uri
+import androidx.lifecycle.viewModelScope
 import com.weit2nd.domain.model.User
 import com.weit2nd.domain.usecase.signup.CheckNicknameDuplicateUseCase
 import com.weit2nd.domain.usecase.signup.VerifyNicknameUseCase
 import com.weit2nd.presentation.base.BaseViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.launch
 import org.orbitmvi.orbit.syntax.simple.intent
 import org.orbitmvi.orbit.syntax.simple.postSideEffect
 import org.orbitmvi.orbit.syntax.simple.reduce
@@ -19,6 +22,7 @@ class SignUpViewModel @Inject constructor(
 ) : BaseViewModel<SignUpState, SignUpSideEffect>() {
 
     override val container = container<SignUpState, SignUpSideEffect>(SignUpState())
+    private var nicknameDuplicateCheckJob: Job = Job().apply { complete() }
 
     fun onSignUpButtonClick() {
         SignUpIntent.RequestSignUp.post()
@@ -34,6 +38,7 @@ class SignUpViewModel @Inject constructor(
 
     fun onNicknameInputValueChange(nickname: String) {
         if (container.stateFlow.value.nickname != nickname) {
+            nicknameDuplicateCheckJob.cancel()
             SignUpIntent.VerifyNickname(nickname).post()
         }
     }
@@ -82,13 +87,25 @@ class SignUpViewModel @Inject constructor(
                         isLoading = true,
                     )
                 }
-                runCatching {
-                    val nicknameState = checkNicknameDuplicateUseCase.invoke(nickname)
-                    reduce {
-                        state.copy(
-                            isLoading = false,
-                            nicknameState = nicknameState,
-                        )
+                nicknameDuplicateCheckJob = viewModelScope.launch {
+                    runCatching {
+                        val nicknameState = checkNicknameDuplicateUseCase.invoke(nickname)
+                        reduce {
+                            state.copy(
+                                isLoading = false,
+                                nicknameState = nicknameState,
+                            )
+                        }
+                    }
+                }.apply {
+                    invokeOnCompletion {
+                        intent {
+                            reduce {
+                                state.copy(
+                                    isLoading = false,
+                                )
+                            }
+                        }
                     }
                 }
             }
