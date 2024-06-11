7e514fad859a8e85705ae56459ba604139ba0885
pingu244 <pingu244@naver.com>
2024-06-11 15:08:05 +0900

    ROFO-24 refactor: 입력 중 커서 위치 변경 문제 해결을 위해 TextFieldValue 사용

--- presentation/src/main/java/com/weit2nd/presentation/ui/signup/SignUpScreen.kt
+++ presentation/src/main/java/com/weit2nd/presentation/ui/signup/SignUpScreen.kt
@@ -15,11 +15,16 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
@@ -151,10 +156,14 @@ fun NicknameTextField(
     isNicknameValid: Boolean,
     onDuplicationBtnClick: () -> Unit,
 ) {
+
+    var userInput by remember { mutableStateOf(TextFieldValue(nickname)) }
+
     TextField(
-        value = nickname,
+        value = userInput,
         onValueChange = { newValue ->
-            onInputValueChange(newValue)
+            userInput = newValue
+            onInputValueChange(newValue.text)
         },
         keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Text),
         placeholder = {
