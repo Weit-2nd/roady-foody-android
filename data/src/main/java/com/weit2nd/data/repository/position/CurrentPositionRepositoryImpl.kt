82fe445730d86929a80a452699c32600b5134646
pingu244 <pingu244@naver.com>
2024-06-15 20:58:14 +0900

    ROFO-31 refactor: 위치 권한이 없거나 위치 정보를 가져올 수 없는 경우 에러 반환

--- data/src/main/java/com/weit2nd/data/repository/position/CurrentPositionRepositoryImpl.kt
+++ data/src/main/java/com/weit2nd/data/repository/position/CurrentPositionRepositoryImpl.kt
@@ -3,17 +3,20 @@ package com.weit2nd.data.repository.position
 import android.annotation.SuppressLint
 import android.os.Looper
 import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationAvailability
 import com.google.android.gms.location.LocationCallback
 import com.google.android.gms.location.LocationRequest
 import com.google.android.gms.location.LocationResult
 import com.google.android.gms.location.Priority
 import com.weit2nd.data.source.position.LocationPermissionDataSource
+import com.weit2nd.domain.exception.LocationException
 import com.weit2nd.domain.model.Location
 import com.weit2nd.domain.repository.position.CurrentPositionRepository
 import kotlinx.coroutines.CancellableContinuation
 import kotlinx.coroutines.suspendCancellableCoroutine
 import javax.inject.Inject
 import kotlin.coroutines.resume
+import kotlin.coroutines.resumeWithException
 
 class CurrentPositionRepositoryImpl @Inject constructor(
     private val locationPermissionDataSource: LocationPermissionDataSource,
@@ -21,14 +24,13 @@ class CurrentPositionRepositoryImpl @Inject constructor(
 ) : CurrentPositionRepository {
 
     override suspend fun getCurrentPosition(): Location {
-        val defaultLocation = Location(latitude = 37.5597706, longitude = 126.9423666)
 
         if (locationPermissionDataSource.requestLocationPermission()) {
             return suspendCancellableCoroutine { continuation ->
                 requestLocationUpdates(continuation)
             }
         }
-        return defaultLocation
+        throw SecurityException("위치 권한이 허용되지 않았습니다.")
     }
 
     // TedPermission.checkGranted()를 통해 권한 허용 확인 완료
@@ -40,6 +42,13 @@ class CurrentPositionRepositoryImpl @Inject constructor(
             .build()
 
         val callback = object : LocationCallback() {
+            override fun onLocationAvailability(availability: LocationAvailability) {
+                if (availability.isLocationAvailable.not()) {
+                    continuation.resumeWithException(LocationException("위치 정보를 받아올 수 없습니다."))
+                    locationClient.removeLocationUpdates(this)
+                }
+            }
+
             override fun onLocationResult(locationResult: LocationResult) {
                 val location = Location(
                     latitude = locationResult.locations.first().latitude,
---
+++ domain/src/main/java/com/weit2nd/domain/exception/LocationException.kt
@@ -0,0 +1,3 @@
+package com.weit2nd.domain.exception
+
+class LocationException(message: String) : Exception(message)
