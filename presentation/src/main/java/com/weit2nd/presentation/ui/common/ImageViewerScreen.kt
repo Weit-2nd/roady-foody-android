9c2e4829ae8e32a854fd9f1146aa69edbfdf020f
pingu244 <pingu244@naver.com>
2024-06-27 23:07:49 +0900

    ROFO-53 feat: 이미지뷰어 화면

---
+++ presentation/src/main/java/com/weit2nd/presentation/ui/common/ImageViewerScreen.kt
@@ -0,0 +1,86 @@
+package com.weit2nd.presentation.ui.common
+
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.pager.HorizontalPager
+import androidx.compose.foundation.pager.rememberPagerState
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.Close
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.unit.dp
+import com.bumptech.glide.integration.compose.ExperimentalGlideComposeApi
+import com.bumptech.glide.integration.compose.GlideImage
+
+@OptIn(ExperimentalFoundationApi::class, ExperimentalGlideComposeApi::class)
+@Composable
+fun ImageViewerScreen(
+    images: List<String>,
+    position: Int,
+) {
+    val pagerState = rememberPagerState(
+        pageCount = { images.size },
+        initialPage = position,
+    )
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.Black)
+    ) {
+        HorizontalPager(state = pagerState) { page ->
+            GlideImage(
+                model = images[page],
+                contentDescription = "$page page Image",
+                modifier = Modifier.fillMaxSize(),
+                contentScale = ContentScale.Fit,
+            )
+        }
+        Row(
+            Modifier
+                .wrapContentHeight()
+                .fillMaxWidth()
+                .align(Alignment.BottomCenter)
+                .padding(bottom = 8.dp),
+            horizontalArrangement = Arrangement.Center
+        ) {
+            repeat(pagerState.pageCount) { index ->
+                val color =
+                    if (pagerState.currentPage == index) Color.White else Color.DarkGray
+                Box(
+                    modifier = Modifier
+                        .padding(4.dp)
+                        .clip(CircleShape)
+                        .background(color)
+                        .size(8.dp)
+                )
+            }
+        }
+        IconButton(
+            modifier = Modifier
+                .padding(4.dp)
+                .align(Alignment.TopStart),
+            onClick = { },
+        ) {
+            Icon(
+                imageVector = Icons.Outlined.Close,
+                contentDescription = "exitPage",
+                tint = Color.White,
+            )
+        }
+    }
+}
