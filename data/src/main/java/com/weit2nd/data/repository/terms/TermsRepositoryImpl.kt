35ee8eb1bbf73ce9e32b94d6622b3b26705a9e83
minseonglove <qnfpdlr124@gmail.com>
2024-06-30 11:17:49 +0900

    ROFO-50 feat: 약관 상세 api 에러 핸들링

--- data/src/main/java/com/weit2nd/data/repository/terms/TermsRepositoryImpl.kt
+++ data/src/main/java/com/weit2nd/data/repository/terms/TermsRepositoryImpl.kt
@@ -3,9 +3,12 @@ package com.weit2nd.data.repository.terms
 import com.weit2nd.data.model.terms.TermDTO
 import com.weit2nd.data.model.terms.TermDetailDTO
 import com.weit2nd.data.source.TermsDataSource
+import com.weit2nd.domain.exception.term.TermIdNotFoundException
 import com.weit2nd.domain.model.terms.Term
 import com.weit2nd.domain.model.terms.TermDetail
 import com.weit2nd.domain.repository.terms.TermsRepository
+import okhttp3.internal.http.HTTP_NOT_FOUND
+import retrofit2.HttpException
 import javax.inject.Inject
 
 class TermsRepositoryImpl @Inject constructor(
@@ -16,9 +19,16 @@ class TermsRepositoryImpl @Inject constructor(
     }
 
     override suspend fun getTermDetail(termId: Long): TermDetail {
-        return dataSource.getTermDetail(
-            id = termId
-        ).toTermDetail()
+        val result = runCatching {
+            dataSource.getTermDetail(
+                id = termId
+            ).toTermDetail()
+        }.onFailure {
+            if (it is HttpException && it.code() == HTTP_NOT_FOUND) {
+                throw TermIdNotFoundException()
+            }
+        }
+        return result.getOrThrow()
     }
 
     private fun List<TermDTO>.toTerms(): List<Term> =
