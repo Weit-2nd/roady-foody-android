b16c3f1c460a242b98dd43c64a7fbd0d43810b63
minseonglove <qnfpdlr124@gmail.com>
2024-07-07 18:56:26 +0900

    ROFO-78 feat: accessToken, refreshToken getter, setter 추가

--- data/src/main/java/com/weit2nd/data/source/auth/AuthDataSource.kt
+++ data/src/main/java/com/weit2nd/data/source/auth/AuthDataSource.kt
@@ -4,7 +4,24 @@ import com.kakao.sdk.auth.AuthApiClient
 import com.weit2nd.domain.exception.auth.AuthException
 
 class AuthDataSource {
+
+    // TODO 토큰 보관 로적 추가 예정. 이렇게 저장해서 쓰면 안됨
+    private var accessToken = ""
+    private var refreshToken = ""
+
     fun getSocialAccessToken(): String =
         AuthApiClient.instance.tokenManagerProvider.manager.getToken()?.accessToken
             ?: throw AuthException.TokenNotFoundException()
+
+    fun setAccessToken(token: String) {
+        accessToken = token
+    }
+
+    fun getAccessToken(): String = accessToken
+
+    fun setRefreshToken(token: String) {
+        refreshToken = token
+    }
+
+    fun getRefreshToken(): String = refreshToken
 }
