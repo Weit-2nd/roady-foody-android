24e2e2aad59629cfd7a5393437a466ed6af737a5
pingu244 <pingu244@naver.com>
2024-07-11 23:56:56 +0900

    ROFO-98 fix: socialAccessToken이 없을 때, 401 Response 반환

--- data/src/main/java/com/weit2nd/data/interceptor/LoginInterceptor.kt
+++ data/src/main/java/com/weit2nd/data/interceptor/LoginInterceptor.kt
@@ -2,14 +2,27 @@ package com.weit2nd.data.interceptor
 
 import com.weit2nd.data.source.auth.AuthDataSource
 import okhttp3.Interceptor
+import okhttp3.Protocol
 import okhttp3.Response
+import okhttp3.ResponseBody.Companion.toResponseBody
 import javax.inject.Inject
 
 class LoginInterceptor @Inject constructor(
     private val dataSource: AuthDataSource,
 ) : Interceptor {
     override fun intercept(chain: Interceptor.Chain): Response {
-        val authorization = BEARER_PREFIX + dataSource.getSocialAccessToken()
+        val socialAccessToken = runCatching {
+            dataSource.getSocialAccessToken()
+        }.getOrElse {
+            return Response.Builder()
+                .request(chain.request())
+                .protocol(Protocol.HTTP_1_1)
+                .code(401)
+                .message("${it.message}")
+                .body(it.stackTraceToString().toResponseBody())
+                .build()
+        }
+        val authorization = BEARER_PREFIX + socialAccessToken
         val newRequest =
             chain.request().newBuilder().apply {
                 addHeader(AUTHORIZATION_HEADER, authorization)
