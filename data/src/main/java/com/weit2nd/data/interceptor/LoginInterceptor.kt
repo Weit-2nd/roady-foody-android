732ee99a7acd447ebd8241632eed03127ba10c6a
pingu244 <pingu244@naver.com>
2024-07-12 19:41:56 +0900

    ROFO-98 refactor: 가독성 향상을 위한 fold 사용

--- data/src/main/java/com/weit2nd/data/interceptor/LoginInterceptor.kt
+++ data/src/main/java/com/weit2nd/data/interceptor/LoginInterceptor.kt
@@ -5,29 +5,34 @@ import okhttp3.Interceptor
 import okhttp3.Protocol
 import okhttp3.Response
 import okhttp3.ResponseBody.Companion.toResponseBody
+import okhttp3.internal.http.HTTP_UNAUTHORIZED
 import javax.inject.Inject
 
 class LoginInterceptor @Inject constructor(
     private val dataSource: AuthDataSource,
 ) : Interceptor {
     override fun intercept(chain: Interceptor.Chain): Response {
-        val socialAccessToken = runCatching {
+        return runCatching {
             dataSource.getSocialAccessToken()
-        }.getOrElse {
-            return Response.Builder()
-                .request(chain.request())
-                .protocol(Protocol.HTTP_1_1)
-                .code(401)
-                .message("${it.message}")
-                .body(it.stackTraceToString().toResponseBody())
-                .build()
-        }
-        val authorization = BEARER_PREFIX + socialAccessToken
-        val newRequest =
-            chain.request().newBuilder().apply {
-                addHeader(AUTHORIZATION_HEADER, authorization)
+        }.fold(
+            onSuccess = { socialAccessToken ->
+                val authorization = BEARER_PREFIX + socialAccessToken
+                val newRequest =
+                    chain.request().newBuilder().apply {
+                        addHeader(AUTHORIZATION_HEADER, authorization)
+                    }
+                chain.proceed(newRequest.build())
+            },
+            onFailure = {
+                Response.Builder()
+                    .request(chain.request())
+                    .protocol(Protocol.HTTP_1_1)
+                    .code(HTTP_UNAUTHORIZED)
+                    .message("${it.message}")
+                    .body(it.stackTraceToString().toResponseBody())
+                    .build()
             }
-        return chain.proceed(newRequest.build())
+        )
     }
 
     companion object {
