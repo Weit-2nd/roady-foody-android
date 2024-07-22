dd83cf5357b2576f3221ea8e4b718cc465d38413
pingu244 <pingu244@naver.com>
2024-07-23 01:27:19 +0900

    ROFO-101 refactor: 음식점 이름 textfield 속성 추가

--- presentation/src/main/java/com/weit2nd/presentation/ui/foodspot/report/FoodSpotReportScreen.kt
+++ presentation/src/main/java/com/weit2nd/presentation/ui/foodspot/report/FoodSpotReportScreen.kt
@@ -14,6 +14,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.selection.selectableGroup
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
@@ -28,11 +29,17 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -172,10 +179,18 @@ private fun NameTextField(
     name: String,
     onNameValueChange: (String) -> Unit,
 ) {
+    var userInput by remember { mutableStateOf(TextFieldValue(name)) }
+
     TextField(
         modifier = Modifier.fillMaxWidth(),
-        value = name,
-        onValueChange = { onNameValueChange(it) },
+        value = userInput,
+        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Text),
+        placeholder = { Text(text = "음식점 이름") },
+        onValueChange = { newValue ->
+            userInput = newValue
+            onNameValueChange(newValue.text)
+        },
+        singleLine = true,
     )
 }
 
