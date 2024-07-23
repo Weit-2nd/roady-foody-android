15ca78c662a0134ee8da75afc64cf14a557e3381
pingu244 <pingu244@naver.com>
2024-07-23 20:56:03 +0900

    ROFO-101 refactor: IconButton 사용

--- presentation/src/main/java/com/weit2nd/presentation/ui/common/CancelableImage.kt
+++ presentation/src/main/java/com/weit2nd/presentation/ui/common/CancelableImage.kt
@@ -1,13 +1,14 @@
 package com.weit2nd.presentation.ui.common
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Close
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -33,12 +34,14 @@ fun CancelableImage(
             contentScale = ContentScale.Crop,
             contentDescription = null,
         )
-        Box(
+
+        IconButton(
             modifier =
                 Modifier
-                    .fillMaxWidth(0.2f)
                     .background(Color.LightGray)
-                    .clickable { onDeleteImage(imgUri) },
+                    .fillMaxWidth(0.2f)
+                    .fillMaxHeight(0.2f),
+            onClick = { onDeleteImage(imgUri) },
         ) {
             Icon(
                 imageVector = Icons.Filled.Close,
