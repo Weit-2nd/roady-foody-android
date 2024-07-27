ed71e9fc616ed2f1823701caba05e54171c0bba6
minseonglove <qnfpdlr124@gmail.com>
2024-07-28 00:38:20 +0900

    ROFO-111 feat: 홈화면 마이페이지 버튼 추가 (임시)

--- presentation/src/main/java/com/weit2nd/presentation/ui/home/HomeScreen.kt
+++ presentation/src/main/java/com/weit2nd/presentation/ui/home/HomeScreen.kt
@@ -3,8 +3,10 @@ package com.weit2nd.presentation.ui.home
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.Button
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Surface
@@ -34,17 +36,22 @@ fun HomeScreen(
         Box(
             modifier = Modifier.fillMaxSize(),
         ) {
-            Text(
-                text = state.value.user.name,
-            )
             MapScreen(modifier = Modifier.fillMaxSize())
             Box(
                 modifier =
                     Modifier
-                        .align(Alignment.BottomStart)
+                        .fillMaxSize()
                         .padding(16.dp),
             ) {
+                Button(
+                    modifier = Modifier.align(Alignment.TopEnd),
+                    onClick = navToMyPage,
+                    shape = RoundedCornerShape(100),
+                ) {
+                    Text(text = "마페")
+                }
                 FloatingActionButton(
+                    modifier = Modifier.align(Alignment.BottomStart),
                     onClick = vm::onClickReportBtn,
                 ) {
                     Icon(Icons.Filled.Add, "report food-spot")
