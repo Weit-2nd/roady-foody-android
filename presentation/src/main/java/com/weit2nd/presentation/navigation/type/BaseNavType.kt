e1549308ddb49456352a6ea026cc37ef6a2c4d9f
minseonglove <qnfpdlr124@gmail.com>
2024-07-30 22:06:05 +0900

    ROFO-114 refactor: BaseNavType 추가

---
+++ presentation/src/main/java/com/weit2nd/presentation/navigation/type/BaseNavType.kt
@@ -0,0 +1,35 @@
+package com.weit2nd.presentation.navigation.type
+
+import android.os.Build
+import android.os.Bundle
+import android.os.Parcelable
+import androidx.navigation.NavType
+import com.google.gson.Gson
+
+abstract class BaseNavType<T : Parcelable>(
+    private val target: Class<T>,
+    override val isNullableAllowed: Boolean = false,
+) : NavType<T>(isNullableAllowed = isNullableAllowed) {
+    override fun get(
+        bundle: Bundle,
+        key: String,
+    ): T? {
+        return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            bundle.getParcelable(key, target)
+        } else {
+            bundle.getParcelable(key)
+        }
+    }
+
+    override fun parseValue(value: String): T {
+        return Gson().fromJson(value, target)
+    }
+
+    override fun put(
+        bundle: Bundle,
+        key: String,
+        value: T,
+    ) {
+        bundle.putParcelable(key, value)
+    }
+}
