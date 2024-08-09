de94d8d7ec4f51b9d1af072f4fdca84820f1a6a3
pingu244 <pingu244@naver.com>
2024-08-09 21:00:21 +0900

    ROFO-146 style: 파라미터 설명 추가

--- domain/src/main/java/com/weit2nd/domain/model/spot/FoodSpotDetail.kt
+++ domain/src/main/java/com/weit2nd/domain/model/spot/FoodSpotDetail.kt
@@ -4,6 +4,20 @@ import java.time.DayOfWeek
 import java.time.LocalDateTime
 import java.time.LocalTime
 
+/**
+ * @param id 음식점 id
+ * @param name 음식점 이름
+ * @param longitude 경도
+ * @param latitude 위도
+ * @param movableFoodSpots 이동여부(푸드 트럭, 푸드 카트 등)
+ * @param openState 운영 상태 [OPEN, CLOSED, TEMPORARILY_CLOSED]
+ * @param storeClosure 폐업 여부
+ * @param operationHoursList 영업 시간 리스트
+ * @param foodCategoryList 음식 카테고리 리스트
+ * @param foodSpotsPhotos 음식점 사진 리스트
+ * @param createdDateTime 음식점 생성 일자
+ */
+
 data class FoodSpotDetail(
     val id: Long,
     val name: String,
