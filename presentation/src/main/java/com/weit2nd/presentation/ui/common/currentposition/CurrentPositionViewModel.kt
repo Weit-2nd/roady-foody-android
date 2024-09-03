ab47fc3e3f50005588392f603602f37d11aff538
minseonglove <qnfpdlr124@gmail.com>
2024-09-03 23:51:05 +0900

    ROFO-200 feat: 현재 위치 재요청 시 기존 요청을 취소

--- presentation/src/main/java/com/weit2nd/presentation/ui/common/currentposition/CurrentPositionViewModel.kt
+++ presentation/src/main/java/com/weit2nd/presentation/ui/common/currentposition/CurrentPositionViewModel.kt
@@ -4,6 +4,7 @@ import com.kakao.vectormap.LatLng
 import com.weit2nd.domain.usecase.position.GetCurrentPositionUseCase
 import com.weit2nd.presentation.base.BaseViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Job
 import org.orbitmvi.orbit.viewmodel.container
 import javax.inject.Inject
 
@@ -14,19 +15,20 @@ class CurrentPositionViewModel @Inject constructor(
     override val container =
         container<CurrentPositionState, CurrentPositionSideEffect>(CurrentPositionState())
 
+    private var currentPositionJob: Job =
+        Job().apply {
+            complete()
+        }
+
     fun onButtonClick() {
-        CurrentPositionIntent.RequestCurrentPosition.post()
+        currentPositionJob.cancel()
+        currentPositionJob = CurrentPositionIntent.RequestCurrentPosition.post()
     }
 
     private fun CurrentPositionIntent.post() =
         intent {
             when (this@post) {
                 CurrentPositionIntent.RequestCurrentPosition -> {
-                    reduce {
-                        state.copy(
-                            isLoading = true,
-                        )
-                    }
                     runCatching {
                         val location = getCurrentPositionUseCase.invoke()
                         val currentPosition =
@@ -37,11 +39,6 @@ class CurrentPositionViewModel @Inject constructor(
                         postSideEffect(CurrentPositionSideEffect.PositionRequestSuccess(currentPosition))
                     }.onFailure {
                         postSideEffect(CurrentPositionSideEffect.PositionRequestFailed(it))
-                    }
-                    reduce {
-                        state.copy(
-                            isLoading = false,
-                        )
                     }
                 }
             }
