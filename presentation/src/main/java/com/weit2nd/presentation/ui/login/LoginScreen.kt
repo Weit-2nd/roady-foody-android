94c27390e4cd9168880911e533dd094dad22f246
pingu244 <pingu244@naver.com>
2024-09-07 09:14:38 +0900

    ROFO-206 refactor: Color 정의

--- presentation/src/main/java/com/weit2nd/presentation/ui/login/LoginScreen.kt
+++ presentation/src/main/java/com/weit2nd/presentation/ui/login/LoginScreen.kt
@@ -20,13 +20,14 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.weit2nd.presentation.R
+import com.weit2nd.presentation.ui.theme.KakaoLoginBackground
+import com.weit2nd.presentation.ui.theme.KakaoLoginText
 import com.weit2nd.presentation.ui.theme.RoadyFoodyTheme
 import org.orbitmvi.orbit.compose.collectAsState
 import org.orbitmvi.orbit.compose.collectSideEffect
@@ -94,7 +95,7 @@ private fun LoginButton(
         modifier =
             modifier
                 .clip(RoundedCornerShape(12.dp))
-                .background(Color(0xFFFEE500))
+                .background(KakaoLoginBackground)
                 .clickable(enabled = enabled, onClick = onClick),
         verticalAlignment = Alignment.CenterVertically,
         horizontalArrangement = Arrangement.Center,
@@ -104,7 +105,7 @@ private fun LoginButton(
             modifier = Modifier.padding(8.dp),
             text = stringResource(id = R.string.login_btn),
             style = MaterialTheme.typography.headlineSmall,
-            color = Color(0xD9000000), // 85% #000000
+            color = KakaoLoginText,
         )
     }
 }
