932974161c6fd202f855a2ee873943cab2350b22
minseonglove <qnfpdlr124@gmail.com>
2024-09-07 21:30:31 +0900

    ROFO-200 feat: 기본 상태바 컬러를 surface로 변경

--- presentation/src/main/java/com/weit2nd/presentation/ui/theme/Theme.kt
+++ presentation/src/main/java/com/weit2nd/presentation/ui/theme/Theme.kt
@@ -87,7 +87,7 @@ fun RoadyFoodyTheme(
     if (!view.isInEditMode) {
         SideEffect {
             val window = (view.context as Activity).window
-            window.statusBarColor = colorScheme.primary.toArgb()
+            window.statusBarColor = colorScheme.surface.toArgb()
             WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = darkTheme
         }
     }
