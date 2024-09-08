2850f67ad8e172616e892236e9717a808f242165
minseonglove <qnfpdlr124@gmail.com>
2024-09-08 15:41:37 +0900

    ROFO-209 feat: 리뷰 아이템 디자인 적용

--- presentation/src/main/java/com/weit2nd/presentation/ui/common/ProfileImage.kt
+++ presentation/src/main/java/com/weit2nd/presentation/ui/common/ProfileImage.kt
@@ -1,25 +1,31 @@
 package com.weit2nd.presentation.ui.common
 
 import android.net.Uri
+import androidx.annotation.DrawableRes
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.painter.ColorPainter
+import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import coil.compose.AsyncImage
 import com.bumptech.glide.integration.compose.ExperimentalGlideComposeApi
-import com.bumptech.glide.integration.compose.GlideImage
 import com.weit2nd.presentation.R
+import com.weit2nd.presentation.ui.theme.Gray4
 
 @OptIn(ExperimentalGlideComposeApi::class)
 @Composable
 fun ProfileImage(
     modifier: Modifier = Modifier,
     imgUri: Uri? = null,
-    defaultImage: Int = R.drawable.ic_launcher_background,
+    @DrawableRes fallback: Int = R.drawable.ic_launcher_background,
+    placeholder: Painter? = ColorPainter(Gray4),
     onProfileImageClick: (() -> Unit) = {},
 ) {
-    GlideImage(
+    AsyncImage(
         model = imgUri,
         contentDescription = "ProfileImage",
         modifier =
@@ -27,7 +33,7 @@ fun ProfileImage(
                 .clip(CircleShape)
                 .clickable { onProfileImageClick() },
         contentScale = ContentScale.Crop,
-    ) {
-        it.fallback(defaultImage)
-    }
+        fallback = painterResource(id = fallback),
+        placeholder = placeholder,
+    )
 }
